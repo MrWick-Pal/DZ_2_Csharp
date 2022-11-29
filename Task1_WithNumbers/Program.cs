43d2474cc4950860b0e5b4cfb69f61622edf46f1
Matrix <cfifgfkm@gmail.com>
2022-11-29 22:49:05 +0300

    DZ done.

---
+++ Task1_WithNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введи трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int a1 = Number / 10;
+Console.WriteLine(a1);
+int a3 = a1 % 10;
+Console.WriteLine(a3);
---
+++ Task1_WithStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введи трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+
+if (stringNumber.Length < 3 || stringNumber.Length > 3)
+{
+    Console.WriteLine("Число не является трёхзначным.");
+}
+else
+{
+    Console.WriteLine("вторая цифра этого числа: " + stringNumber[1]);
+}
